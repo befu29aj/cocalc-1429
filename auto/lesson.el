319bb69cf15562390535ab1aa198bdb92e10b9cb
Stefan Lukits <sediomyle@gmail.com>
2017-11-09 08:13:15 -0800

    ft-10-ProductQuotient.tex

--- auto/lesson.el
+++ auto/lesson.el
@@ -3,14 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,30 +31,34 @@
     "qeq"
     "QEQ")
    (LaTeX-add-labels
-    "eq:eeheemei"
-    "eq:fuazegho"
-    "eq:iivoixah"
-    "eq:aipheiyu"
-    "eq:adailaek"
-    "eq:aizienah"
-    "eq:eicaemae"
-    "eq:xohsheco"
-    "eq:zeiquahn"
-    "eq:kaitoohe"
-    "eq:ainguxuf"
-    "eq:kahdaixi"
-    "eq:adeongie"
-    "eq:aiyohboo"
-    "eq:veatoiyu"
-    "eq:kohvoigh"
-    "eq:leiweidi"
-    "eq:aewailei"
-    "eq:eezeisud"
-    "eq:utheikuv"
-    "eq:beizooph"
-    "eq:ieyuipoo"
-    "eq:maighere"
-    "eq:eeleeyae")
+    "eq:aepuaxai"
+    "eq:yahtiera"
+    "eq:piebaith"
+    "eq:eceishie"
+    "eq:chiekeey"
+    "eq:vaeyiewe"
+    "eq:iefeiwah"
+    "eq:recootie"
+    "eq:roothubu"
+    "eq:iebowohc"
+    "eq:iilahthi"
+    "eq:eshohhoo"
+    "eq:iophiewu"
+    "eq:uhushain"
+    "eq:requuare"
+    "eq:xookaeji"
+    "eq:eidoogow"
+    "eq:ciedaeme"
+    "eq:ohquailo"
+    "eq:seemaxah"
+    "eq:iebieluk"
+    "eq:eeghaphe"
+    "eq:quanoefe"
+    "eq:oozeexei"
+    "eq:eipovaif"
+    "eq:ejahshai"
+    "eq:nooshahw"
+    "eq:weeshieb")
    (LaTeX-add-counters
     "expls"
     "exercise")
