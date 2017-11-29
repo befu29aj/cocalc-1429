7c0493cadd8060567e97bcbe4169c3d051aaf33f
Stefan Lukits <sediomyle@gmail.com>
2017-11-29 10:08:47 -0800

    i

---
+++ auto/gm-16-SphericalTrigonometry.el
@@ -0,0 +1,39 @@
+(TeX-add-style-hook
+ "gm-16-SphericalTrigonometry"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "tikz"
+    "cancel"
+    "graphicx"
+    "wrapfig"
+    "colortbl"
+    "color"
+    "alltt")
+   (TeX-add-symbols
+    '("ubung" 0)
+    '("beispiel" 1)
+    "CourseName"
+    "CourseNumber"
+    "CourseInst"
+    "qeq"
+    "QEQ")
+   (LaTeX-add-labels
+    "eq:aeceiwae"
+    "eq:theeyoom"
+    "eq:cheichah"
+    "eq:yooceeva"
+    "eq:epheepee"
+    "eq:dijeeghe"
+    "eq:aechiedu"
+    "eq:aebaeyun"
+    "eq:uteseivu")
+   (LaTeX-add-counters
+    "expls"
+    "exercise"))
+ :latex)
+
