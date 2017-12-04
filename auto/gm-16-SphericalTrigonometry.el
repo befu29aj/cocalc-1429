5ba3c4694294661bb679507c0dddc3e6081356db
Stefan Lukits <sediomyle@gmail.com>
2017-12-04 11:05:56 -0800

    gm-16-SphericalTrigonometry.tex

--- auto/gm-16-SphericalTrigonometry.el
+++ auto/gm-16-SphericalTrigonometry.el
@@ -3,6 +3,15 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -34,6 +43,9 @@
     "eq:aebaeyun")
    (LaTeX-add-counters
     "expls"
-    "exercise"))
+    "exercise")
+   (LaTeX-add-xcolor-definecolors
+    "red"
+    "myblue"))
  :latex)
 
